43cc6059725570b88166b22b76c7a9264dddc7b7
DeAdFrOnt <semenukmaks@gmail.com>
2020-08-16 12:01:14 +0300

    feat: render btn of answers fix: QuestionBox attribute name 'options'

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
-// create single function which brings in props using a single argument(from dataArray)
-const QuestionBox = ({question, options}) =>{
-    return(
-        // destructuring - expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables
-        // instead argument we destructuring directly to exctract question and options
-        // then return jsx template
-        <div className="questionBox">
-            <div className="question">{question}</div>
-        </div>
-    )
-}
+const QuestionBox = ({ question, options }) => {
+  // when user press one of the btn we wanna remove other from UI
+  // using Local State Variable to hold options Array when QuestionBox component created
+  // we can use it to render a set of option btn
+  // function component dont provide state management as class component
+  // cause we dont wanna state management and lifecycle feature in component which use to only display data and provide simple interaction features
+  // if u will need state in function component react offers plug&play API known as HOOKS
+  // import useState and create state var set initial value of answer var from options array that getting as prop
 
-export default QuestionBox
+  const [answer, setAnswer] = useState(options);
+
+  // setAnswer refers to funct that can be used to update value of answer var
+  // use .map on the answer var to render series of bn
+  // use index from answer array as key
+  return (
+    <div className="questionBox">
+      <div className="question">{question}</div>
+      {answer.map((text, index) => (
+        <button key={index} className="answerBtn">
+          {text}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default QuestionBox;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import "./assets/style.css";
 import quizService from "./quizService";
 import QuestionBox from "./components/QuestionBox";
 
-// creating class component QuizBee
 class QuizBee extends Component {
   state = {
-    // here question store when pulled from API
-
-    // state should always be located at the nearest parent cause easy to debug
     questionBank: [],
   };
-
-  // only way to update state variable by setState
   getQuestions = () => {
     quizService().then((question) => {
       this.setState({
@@ -21,7 +15,6 @@ class QuizBee extends Component {
       });
     });
   };
-  // we need to run function when component load up thats why componentDidMount
   componentDidMount() {
     this.getQuestions();
   }
@@ -29,20 +22,13 @@ class QuizBee extends Component {
     return (
       <div className="container">
         <div className="title">QuizBee</div>
-        {/* cheking is questionBank does have data to render */}
-        {/* its will render a data only when its available {this.state.questionBank.length>0}*/}
-        {/* use .map function to replace question objects */}
-        {/* and then return back with => h4 tags with question text */}
-
-        {/* instead of h4 we put QuestionBox with two props which automaticly bound question to render question and options to render btn of answers  */}
-        {/* and property called key equal to questionId from data so React identify and correlate example of component with data thats consumes */}
 
         {this.state.questionBank.length > 0 &&
           this.state.questionBank.map(
             ({ question, answers, correct, questionId }) => (
               <QuestionBox
                 question={question}
-                option={answers}
+                options={answers}
                 key={questionId}
               />
             )
